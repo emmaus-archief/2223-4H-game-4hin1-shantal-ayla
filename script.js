068fa10305f33dfa5e82c90c18db11998a346b45
shantalh <115077424+shantalh@users.noreply.github.com>
2023-03-08 11:30:03 +0000

    background + function

--- script.js
+++ script.js
@@ -31,7 +31,10 @@ var spelerY = 600; // y-positie van speler
  */
 var beweegAlles = function() {
   // speler
-
+ if (keyIsDown(65)) {
+   spelerX = spelerX+1;
+ }
+  
   // vijand
 
   // kogel
@@ -56,7 +59,8 @@ var verwerkBotsing = function() {
  */
 var tekenAlles = function() {
   // achtergrond
-
+fill("blue");
+  rect(0,0,1280,720);
   // vijand
 
   // kogel
