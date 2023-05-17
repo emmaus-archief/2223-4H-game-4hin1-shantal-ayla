13b061f35d8106df6b4434113f42b85da6eff0c5
shantalh <115077424+shantalh@users.noreply.github.com>
2023-05-17 10:08:38 +0000

    werkt

--- script.js
+++ script.js
@@ -15,23 +15,29 @@
 /* ********************************************* */
 /* globale variabelen die je gebruikt in je game */
 /* ********************************************* */
+var aantal = 0;
+
 const SPELEN = 1;
 const GAMEOVER = 2;
 const UITLEG = 8;
-var spelStatus = SPELEN;
+var spelStatus = UITLEG;
+
 const KEY_LEFT = 37;
 const KEY_RIGHT = 39;
 const KEY_UP = 38;
 const KEY_DOWN = 40;
 
-var spelerX = 600; // x-positie van speler
-var spelerY = 600; // y-positie van speler
+var spelerX = 100; // x-positie van speler
+var spelerY = 360; // y-positie van speler
 var vijandX = 600; // x-positie van vijand
 var vijandY = 500; // y-positie van vijand
+var vijand2X = 700; // x-positie van vijand
+var vijand2Y = 190; // y-positie van vijand
 
 var img; //plaatje
 var imghaai; //plaatje
 var imgachter; //plaatje
+
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
 /* ********************************************* */
@@ -41,17 +47,14 @@ var imgachter; //plaatje
  */
 var beweegAlles = function() {
   // speler
- if (keyIsDown(KEY_LEFT)) {
-   spelerX = spelerX-1;
- }
 if (keyIsDown(KEY_RIGHT)) {
-   spelerX = spelerX+1;
+   spelerX = spelerX+2;
  }
   if (keyIsDown(KEY_UP)) {
-   spelerY = spelerY-1;
+   spelerY = spelerY-2;
  }
   if (keyIsDown(KEY_DOWN)) {
-   spelerY = spelerY+1;
+   spelerY = spelerY+2;
  }
   
 
@@ -67,13 +70,7 @@ if (keyIsDown(KEY_RIGHT)) {
  */
 var verwerkBotsing = function() {
   // botsing speler tegen vijand
-  if (spelerX - vijandX < 50 &&
-     spelerX - vijandX >-50 &&
-     spelerY - vijandY <50 &&
-     spelerY - vijandY > -50) {
-    console.log("Botsing");
-     }
-
+   
   // botsing kogel tegen vijand
 
   // update punten en health
@@ -85,28 +82,32 @@ var verwerkBotsing = function() {
  */
 var tekenAlles = function() {
   // achtergrond
-fill('blue');
-  rect(0,0, width, height);
-    image (imgachter, 0, 0, 600, 600);
+    fill('blue');
+    rect(0,0, width, height);
+    image (imgachter, 0, 0, 1280, 720);
   
   // vijand
-  image(img, vijandX-25, vijandY-25, 65, 65);
+  fill("red");
+  rect(vijandX - 25, vijandY - 25, 50, 50);
+  fill("black");
+  ellipse(vijandX, vijandY, 10, 10);
+  image(img, vijandX - 37, vijandY - 60, 110, 110);
 
+  // vijand 2
+  fill("red");
+  rect(vijand2X - 25, vijand2Y - 25, 50, 50);
+  fill("black");
+  ellipse(vijand2X, vijand2Y, 10, 10);
+  image(img, vijand2X - 37, vijand2Y - 60, 110, 110);
 
-
-
-    
-
-
-  
-  // kogel
+  // munten
 
   // speler
   fill("white");
   rect(spelerX - 25, spelerY - 25, 50, 50);
   fill("black");
-  ellipse(spelerX, spelerY, 10, 10);
-  image (imghaai, spelerX-25, spelerY-25, 65, 65);
+  ellipse (spelerX, spelerY, 10, 10);
+  image (imghaai, spelerX - 100, spelerY - 100, 200, 200);
   // punten en health
 
 };
@@ -116,12 +117,21 @@ fill('blue');
  * anders return false
  */
 var checkGameOver = function() {
-  if(spelerX - vijandX < 50 &&
-     vijandX - spelerX < 50 &&
-     spelerY - vijandY < 50 &&
-     vijandY - spelerY < 50) {
+  if(spelerX - vijandX < 130 &&
+     vijandX - spelerX < 130 &&
+     spelerY - vijandY < 130 &&
+     vijandY - spelerY < 130) {
     aantal = aantal + 1;
     console.log ("botsing"+ aantal)
+    return true;
+     }
+  if(spelerX - vijand2X < 130 &&
+     vijand2X - spelerX < 130 &&
+     spelerY - vijand2Y < 130 &&
+     vijand2Y - spelerY < 130) {
+    aantal = aantal + 1;
+    console.log ("botsing"+ aantal)
+    return true;
      }
   // check of HP 0 is , of tijd op is, of ...
   return false;
@@ -169,14 +179,31 @@ function draw() {
     if (checkGameOver()) {
       spelStatus = GAMEOVER;
     }
+    console.log("spelen");
   }
+  
   if (spelStatus === GAMEOVER) {
     // teken game-over scherm
-
+       console.log("game over");
+    textSize(50);
+    fill("white");
+    text("game over, druk spatie om opnieuw te spelen", 100, 100);
+    if (keyIsDown(32)) { //spatie
+    spelStatus = UITLEG;
+    }
   }
   if (spelStatus === UITLEG) {
     // teken uitleg scherm
-
+       console.log("uitleg");
+       textSize(50);
+       fill("blue");
+    rect(0,0, 1280, 720);
+    fill ("white");
+    text("uitleg: Vermijd de bommen en eet de visjes op! \nDruk op enter om te beginnen.", 50, 350);
+    if (keyIsDown(13)) { // enter
+      spelerX = 100;
+      spelStatus = SPELEN;
+    }
 }
 
 }
